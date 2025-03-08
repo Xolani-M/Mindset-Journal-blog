a78d6e8bddd67fb32c802bde2b4f36445a7789a5
sageMode <nathanielmvana@gmail.com>
2025-03-08 14:01:50 +0200

    initial commit

---
+++ src/main/java/com/mindsetjournal/MindsetJournalApplication.java
@@ -0,0 +1,13 @@
+package com.mindsetjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindsetJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindsetJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindsetjournal/MindsetJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindsetjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsetJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
