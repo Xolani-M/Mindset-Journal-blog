e238f789a6e678d6fbe7dd457a7644951022c20a
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:07:40 +0200

    Add CategoryController.java

--- src/main/java/com/mindsetjournal/controllers/CategoryController.java
+++ src/main/java/com/mindsetjournal/controllers/CategoryController.java
@@ -1,4 +1,31 @@
 package com.mindsetjournal.controllers;
 
+import com.mindsetjournal.domain.dtos.CategoryDTO;
+import com.mindsetjournal.domain.entities.Category;
+import com.mindsetjournal.mappers.CategoryMapper;
+import com.mindsetjournal.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/api/v1/categories")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> listCategories() {
+        List<CategoryDTO> categories = categoryService.listCategories()
+                .stream().map(categoryMapper::toDto)
+                .toList();
+        return ResponseEntity.ok(categories);
+    }
 }
