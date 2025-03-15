b0c91ee50e5354486acc722ad312b8e233e00a45
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:09:06 +0200

    Addd PostRepository.java

--- src/main/java/com/mindsetjournal/repository/PostRepository.java
+++ src/main/java/com/mindsetjournal/repository/PostRepository.java
@@ -1,4 +1,11 @@
 package com.mindsetjournal.repository;
 
-public interface PostRepository {
+import com.mindsetjournal.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
 }
