ec97074aa0e5caf7706b1efdb4e4e4053b32ac33
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:07:57 +0200

    Add CategoryDTO.java

--- src/main/java/com/mindsetjournal/domain/dtos/CategoryDTO.java
+++ src/main/java/com/mindsetjournal/domain/dtos/CategoryDTO.java
@@ -1,4 +1,19 @@
 package com.mindsetjournal.domain.dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
+    private UUID id;
+    private String name;
+    private long postCount;
 }
