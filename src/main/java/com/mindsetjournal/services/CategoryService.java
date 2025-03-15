7f690d9eb368ac41b76ea51c92e8f944d73b7e2e
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:56:50 +0200

    Add method to check if a category exists and update createCategory method

--- src/main/java/com/mindsetjournal/services/CategoryService.java
+++ src/main/java/com/mindsetjournal/services/CategoryService.java
@@ -1,4 +1,10 @@
 package com.mindsetjournal.services;
 
+import com.mindsetjournal.domain.entities.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<Category> listCategories();
+    Category createCategory(Category category);
 }
