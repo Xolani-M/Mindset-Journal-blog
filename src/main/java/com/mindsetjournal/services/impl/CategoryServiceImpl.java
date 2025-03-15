aae3f3e46f5cda1fb37a0997a00b59f0df5e4463
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:53:55 +0200

    Implement createCategory method in CategoryServiceImpl

--- src/main/java/com/mindsetjournal/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/mindsetjournal/services/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.mindsetjournal.services.impl;
 import com.mindsetjournal.domain.entities.Category;
 import com.mindsetjournal.repository.CategoryRepository;
 import com.mindsetjournal.services.CategoryService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,16 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public Category createCategory(Category category) {
+        String categoryName = category.getName();
+        if (categoryRepository.existsByNameIgnoreCase(category.getName())) {
+            throw new IllegalArgumentException("Category name already exists with name: " + categoryName);
+        }
+        return categoryRepository.save(category);
+
     }
 }
