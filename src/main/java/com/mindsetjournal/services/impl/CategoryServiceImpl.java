bb6a4eaa104b8f1547b0e8aff05a051755f58097
sageMode <nathanielmvana@gmail.com>
2025-03-15 15:10:06 +0200

    Add CategoryServiceImpl.java

--- src/main/java/com/mindsetjournal/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/mindsetjournal/services/impl/CategoryServiceImpl.java
@@ -1,4 +1,22 @@
 package com.mindsetjournal.services.impl;
 
-public class CategoryServiceImpl {
+import com.mindsetjournal.domain.entities.Category;
+import com.mindsetjournal.repository.CategoryRepository;
+import com.mindsetjournal.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> listCategories() {
+        return categoryRepository.findAllWithPostCount();
+    }
 }
